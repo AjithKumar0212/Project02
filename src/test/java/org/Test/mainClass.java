ef4243b63a8cbdc0470648958e5b0f347c23c423
Ajithkumar0212 <Ajithniran07@gmail.com>
2022-04-21 21:37:58 +0530

    Ajith

--- src/test/java/org/Test/mainClass.java
+++ src/test/java/org/Test/mainClass.java
@@ -8,11 +8,6 @@ import org.testng.annotations.Test;
 public class mainClass extends BaseClass {
 @Test
 private void t2() throws InterruptedException {
-System.out.println("yes");
-System.out.println("2sw");
-System.out.println("yezz");
-System.out.println("1234");
-System.out.println("foggy");
 launchBrowser("chrome");
 launchUrl("https://www.flipkart.com");
 FirstPage f=new FirstPage();
